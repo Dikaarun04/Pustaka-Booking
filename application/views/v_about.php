b8dd79dac245c17783e49c391ac736f2e3bb38ec
dikaarunazilputra <dikaarun04@gmail.com>
2022-10-30 15:39:25 +0700

    Menambah Data Siswa

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+     <h1><?php echo $judul ?></h1>
+     <h4>Nama</h4>
+     <ul type="disc">
+          <li>Nama Depan : Dika Arun</li>
+          <li>Nama Belakang : Azilputra</li>
+     </ul>
+     <br>
+     <h4>Alamat</h4>
+     <ul type="none">
+          <li> Desa Banjaratma Kecamatan Bulakamba Kabupaten Brebes</li>
+     </ul>
+
+     <h4>Tempat Lahir</h4>
+     <ul type="none">
+          <li>Brebes</li>
+     </ul>
+
+     <h4>Olah Raga Favorit</h4>
+     <ul type="square">
+          <li>Futsal</li>
+          <li>Renang</li>
+     </ul>
+</section>
